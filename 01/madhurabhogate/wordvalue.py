97283248fa0402fde23db94b3a83a921820fdcdb
Madhura Bhogate <madhurabhogate@Madhuras-MacBook-Pro.local>
2017-12-20 23:19:59 -0800

    PCC01 madhurabhogate

--- 01/madhurabhogate/wordvalue.py
+++ 01/madhurabhogate/wordvalue.py
@@ -2,17 +2,20 @@ from data import DICTIONARY, LETTER_SCORES
 
 def load_words():
     """Load dictionary into a list and return list"""
-    pass
+    with open(DICTIONARY) as file:
+        return [word.strip() for word in file.read().splitlines()]
 
-def calc_word_value():
+def calc_word_value(word):
     """Calculate the value of the word entered into function
     using imported constant mapping LETTER_SCORES"""
-    pass
+    return sum(LETTER_SCORES.get(char.upper(), 0) for char in word)
 
-def max_word_value():
+def max_word_value(word_list=None):
     """Calculate the word with the max value, can receive a list
     of words as arg, if none provided uses default DICTIONARY"""
-    pass
+    if word_list is None:
+        word_list = load_words()
+    return max(word_list, key=calc_word_value)
 
 if __name__ == "__main__":
     pass # run unittests to validate
